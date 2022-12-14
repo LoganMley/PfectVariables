8e11ff6496ef009abe85007264ec774d11c84b14
Logan Miley <loganmiley4@gmail.com>
2022-12-14 14:08:47 +0400

    ChangesForLogin

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import AboutScreen from "./screens/AboutScreen";
-import ContactScreen from "./screens/ContactScreen";
-import HomeScreen from "./screens/HomeScreen";
-import PricingScreen from "./screens/PricingScreen";
-import LogInScreen from "./screens/LogInScreen";
-import LayoutRoute from "./LayoutRoute";
+import React from 'react';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import AboutScreen from './screens/AboutScreen';
+import ContactScreen from './screens/ContactScreen';
+import RegistrationScreen from './screens/RegistrationScreen';
+import LoginScreen from './screens/LoginScreen';
+import LayoutRoute from './LayoutRoute';
+import GuestLayoutRoute from './GuestLayoutRoute';
+import PrivateLayoutRoute from './PrivateLayoutRoute';
 
 
 function App() {
-  return(
-      <BrowserRouter>
-          <Switch>
-              <LayoutRoute path="/" exact={true} component={HomeScreen} />
-              <LayoutRoute path="/about" exact={true} component={AboutScreen} />
-              <LayoutRoute path="/pricing" exact={true} component={PricingScreen} />
-              <LayoutRoute path="/contact" exact={true} component={ContactScreen} />
-              <LayoutRoute path="/login" exact={true} component={LogInScreen} />
-          </Switch>
-      </BrowserRouter>
-  )
-
+    return(
+        <BrowserRouter>
+            <Switch>
+                <LayoutRoute path="/" exact={true} component={HomeScreen} />
+                <LayoutRoute path="/about" exact={true} component={AboutScreen} />
+                <LayoutRoute path="/contact" exact={true} component={ContactScreen} />
+                <LayoutRoute path="/register" exact={true} component={RegistrationScreen} />
+                <LayoutRoute path="/login" exact={true} component={LoginScreen} />
+            </Switch>
+        </BrowserRouter>
+    )
 }
 
 export default App;
