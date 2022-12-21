93841205b1e919de6e4e3a2c222eb7b4d26fa202
Maxat <116979864+MAXAT22@users.noreply.github.com>
2022-12-21 16:37:22 +0400

    Update AboutScreen.js

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,18 +1,19 @@
-import Header from "./Header";
-
 function AboutScreen() {
-    return (
-        <div>
-        <div className='container'>
-       <h1>About Us</h1>
-       <p>Hello</p>
-       </div>
-       
-       
+  return (
+    <div>
+        <div className="container">
+          <h1>About us</h1>
+          <p>A bookstore is a place where people come to buy
+             educational books, textbooks, informational or gift publications, 
+             as well as in search of literature for cultural leisure or reading 
+             in the subway and trains. This determines the characteristic composition
+              of clients, and is a goal-forming factor.</p>
+        </div>
     </div>
-    );
-  }
-  
-  export default AboutScreen;
+  );
+}
+
+export default AboutScreen;
+
   
   
