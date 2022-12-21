68955893116204bdba8338ac45dd482c3934a275
Maxat <116979864+MAXAT22@users.noreply.github.com>
2022-12-21 19:16:54 +0400

    Update Hero.js

--- src/screens/Hero.js
+++ src/screens/Hero.js
@@ -10,39 +10,51 @@ import Button from "@mui/material/Button";
 function Hero() {
   return (
     <Box
-    sx={{
-      bgcolor: 'background.paper',
-      pt: 8,
-      pb: 6,
-    }}
-  >
-    <Container maxWidth="sm">
-      <Typography
-        component="h1"
-        variant="h2"
-        align="center"
-        color="text.primary"
-        gutterBottom
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+
+function Hero() {
+    return (
+        <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6,
+        }}
       >
-        Album layout
-      </Typography>
-      <Typography variant="h5" align="center" color="text.secondary" paragraph>
-        Something short and leading about the collection below—its contents,
-        the creator, etc. Make it short and sweet, but not too short so folks
-        don&apos;t simply skip over it entirely.
-      </Typography>
-      <Stack
-        sx={{ pt: 4 }}
-        direction="row"
-        spacing={2}
-        justifyContent="center"
-      >
-        <Button variant="contained">Main call to action</Button>
-        <Button variant="outlined">Secondary action</Button>
-      </Stack>
-    </Container>
-  </Box>
-  );
-}
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            DUBAI BOOKS
+          </Typography>
+          <Typography variant="h5" align="center" color="red" paragraph>
+           Welcome to Dubai books. Here you will have access 
+           to 1000 books of different types and genres. You can 
+           buy and read them from the comfort of your home. Just be part of Dubai books
+          </Typography>
+          <Stack
+            sx={{ pt: 4 }}
+            direction="row"
+            spacing={3}
+            justifyContent="center"
+          >
+            <Button variant="contained">Be a part</Button>
+            <Button variant="contained">More info</Button>
+            <Button variant="contained">Subscribe</Button>
+            <Button variant="contained">Lets start</Button>
+          </Stack>
+        </Container>
+      </Box>
+    )
+};
 
 export default Hero;
